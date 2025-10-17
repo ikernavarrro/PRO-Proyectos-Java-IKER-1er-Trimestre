c31e452abe77cb0df4e0363d59f99a06b597ff5e
Iker Navarro Pérez <iker.navarro.perez@zabalburu.org>
2025-10-17 21:15:50 +0200

    Completada Actividad 4_5

--- Actividad4_5/src/main/java/org/zabalburu/daw1/actividad4_5/Actividad4_5.java
+++ Actividad4_5/src/main/java/org/zabalburu/daw1/actividad4_5/Actividad4_5.java
@@ -4,6 +4,7 @@
 
 package org.zabalburu.daw1.actividad4_5;
 
+import javax.swing.JOptionPane;
 import org.zabalburu.daw1.actividad4_5.modelo.Alumno;
 
 /**
@@ -14,6 +15,12 @@ public class Actividad4_5 {
 
     public static void main(String[] args) {
         Alumno daw1 = new Alumno();
+        JOptionPane.showMessageDialog(null, """
+                                            <html>
+                                                <h1>Actividad 4_5 Calcular Estadísticas de las Notas de una Serie de Alumnos.</h1>
+                                                <h5>Desarrollado por Iker Navarro Pérez</h5>
+                                            </html>
+                                            """, "Bienvenid@", JOptionPane.INFORMATION_MESSAGE);
         daw1.procesarAlumnos();
     }
 }
--- Actividad4_5/src/main/java/org/zabalburu/daw1/actividad4_5/modelo/Alumno.java
+++ Actividad4_5/src/main/java/org/zabalburu/daw1/actividad4_5/modelo/Alumno.java
@@ -11,28 +11,55 @@ import javax.swing.JOptionPane;
  * @author Iker Navarro Pérez
  */
 public class Alumno {
+
     public int contadorAlumnos = 0; //Contador
     public int alumnosAprobados = 0; //Contador
     public double sumaNotas = 0; //Acumulador
-    public static final int PROCESAR_SI = 1;
-    public static final int PROCESAR_NO = 0;
-    
-    public void procesarAlumnos(){
-        
-   
-        while(PROCESAR_SI == Integer.parseInt(JOptionPane.showInputDialog(null, """
-                                                                                """))){
-        double notaAlumnoProcesado = Double.parseDouble(JOptionPane.showInputDialog(null, """
+
+    public void procesarAlumnos() {
+        while (JOptionPane.YES_OPTION == JOptionPane.showConfirmDialog(null, """
+                                                                            <html>
+                                                                                <h1>¿Desea procesar un alumno?</h1>
+                                                                                <hr />
+                                                                                <h4>[SI: Introducir Nuevo Alumno | NO: Mostrar Estadísticas]</h4>
+                                                                            </html>
+                                                                            """, "Procesar Alumno", JOptionPane.YES_NO_OPTION)) {
+
+            double notaAlumnoProcesado = Double.parseDouble(JOptionPane.showInputDialog(null, """
                                                                                      <html>
                                                                                         <hr />
                                                                                         <h1>Introduzca la Nota del Nuevo Alumno</h1>
                                                                                         <hr />
                                                                                         <h3>Alumnos Registrados: %d</h3>
                                                                                         <hr />
+                                                                                        <p> [Introduzca -1 para volver atrás.] </p> 
+                                                                                        <hr />
                                                                                         <h5>Desarrollado por Iker Navarro Pérez<h5>
                                                                                      </html>
                                                                                      """.formatted(contadorAlumnos), ""));
-        
+            if (notaAlumnoProcesado != -1) {
+                contadorAlumnos++;
+                if (notaAlumnoProcesado >= 5) {
+                    alumnosAprobados++;
+                }
+                sumaNotas += notaAlumnoProcesado;
+            }
         }
+        
+        double notaMedia = sumaNotas / contadorAlumnos;
+        
+        JOptionPane.showMessageDialog(null, """
+                                            <html>
+                                                <hr />
+                                                <h1>Estadísticas de las Notas</h1>
+                                                <hr />
+                                                <p>
+                                                    Alumnos Examinados: %d <br />
+                                                    Alumnos Aprobados: %d  <br />  
+                                                    Nota Media de los Alumnos: %.2f
+                                                </p>
+                                                <hr />
+                                            </html>
+                                            """.formatted(contadorAlumnos, alumnosAprobados, notaMedia));
     }
 }
