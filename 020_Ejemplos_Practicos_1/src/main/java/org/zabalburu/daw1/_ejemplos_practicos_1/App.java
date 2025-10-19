c9e28fda161d2d433b0b64a79db3ae660bb7e2bc
Iker Navarro Pérez <iker.navarro.perez@zabalburu.org>
2025-10-19 20:55:45 +0200

    Creado 020_Ejemplos_Prácticos_1

---
+++ 020_Ejemplos_Practicos_1/src/main/java/org/zabalburu/daw1/_ejemplos_practicos_1/App.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.zabalburu.daw1._ejemplos_practicos_1;
+
+/**
+ *
+ * @author Iker Navarro Pérez
+ */
+public class App {
+
+    public static void main(String[] args) {
+        
+    }
+}
