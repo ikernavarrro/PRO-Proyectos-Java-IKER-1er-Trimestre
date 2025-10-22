06976afb53be5d0ee7c1e69578ed177c5845049c
Iker Navarro <iker.navarro.perez@zabalburu.org>
2025-10-22 12:17:33 +0200

    Creada Actividad4_2

---
+++ Actividad4_2/src/main/java/org/zabalburu/daw1/actividad4_2/Actividad4_2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.zabalburu.daw1.actividad4_2;
+
+/**
+ *
+ * @author Iker Navarro Pérez
+ */
+public class Actividad4_2 {
+
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ Actividad4_2/src/main/java/org/zabalburu/daw1/actividad4_2/modelo/Empleado.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.zabalburu.daw1.actividad4_2.modelo;
+
+/**
+ *
+ * @author DAW1
+ */
+public class Empleado {
+    
+    public String nombre;
+    public int tipo;
+    public int hijos;
+    public double pluses;
+    
+}
