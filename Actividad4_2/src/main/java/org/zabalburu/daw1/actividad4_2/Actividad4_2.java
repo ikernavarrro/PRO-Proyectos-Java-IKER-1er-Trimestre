7df16f8104e1a1d173df209a0d4d2eeaacc42846
Iker Navarro Pérez <iker.navarro.perez@zabalburu.org>
2025-10-22 20:17:02 +0200

    Completada Actividad4_2 Alternativas Y Repetitivas

--- Actividad4_2/src/main/java/org/zabalburu/daw1/actividad4_2/Actividad4_2.java
+++ Actividad4_2/src/main/java/org/zabalburu/daw1/actividad4_2/Actividad4_2.java
@@ -1,8 +1,10 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package org.zabalburu.daw1.actividad4_2;
+
+import javax.swing.JOptionPane;
+import org.zabalburu.daw1.actividad4_2.modelo.Empleado;
 
 /**
  *
@@ -12,5 +14,98 @@ public class Actividad4_2 {
 
     public static void main(String[] args) {
         
+        Empleado xx = new Empleado();
+        String resp = "";
+        
+        do {
+            xx.nombre = JOptionPane.showInputDialog(null, 
+                    """
+                    <html>
+                        <h1>Introduzca el Nombre de un Nuevo Empleado:</h1> 
+                    </html>
+                    """);
+            
+            resp = JOptionPane.showInputDialog(null, 
+                    """
+                    <html>
+                        <h1>Introduzca el Tipo del Nuevo Empleado:</h1> 
+                        <hr />
+                        <p>
+                            1 : Operario <br />
+                            2 : Obrero Especialista <br />
+                            3 : Administrativo <br />
+                            4 : Licenciado
+                        </p>
+                    </html>
+                    """);
+            xx.tipo = Integer.parseInt(resp);
+            
+            resp = JOptionPane.showInputDialog(null, 
+                    """
+                    <html>
+                        <h1>Introduzca el Número de hijos del Nuevo Empleado:</h1> 
+                    </html>
+                    """);
+            xx.hijos = Integer.parseInt(resp);
+            
+            JOptionPane.showMessageDialog(null, 
+                    """
+                    <html>
+                        <hr />
+                        <p>
+                            Nombre: %s <br />
+                            Tipo: %s <br />
+                            Hijos: %d <br />
+                            Sueldo Base: %.0f€
+                        </p>
+                        <hr />
+                    </html>
+                    """.formatted(xx.nombre, xx.getDescripción(), xx.hijos, xx.getSueldoBase()),
+                    "Datos de %s".formatted(xx.nombre),
+                    JOptionPane.INFORMATION_MESSAGE);
+            
+            do{
+                resp = JOptionPane.showInputDialog(null, 
+                    """
+                    <html>
+                        <h1>Introduzca la cantidad del PLUS para el Nuevo Empleado:</h1> 
+                        <hr />
+                        <p>
+                            (Introduzca una cantidad IGUAL a 0 para CONTINUAR sin agregar nada más).
+                        </p>
+                    </html>
+                    """);
+                xx.nuevoPlus(Integer.parseInt(resp)); // Llamámos al método que suma automáticamente los pluses y Convertimos la respuesta del usuario a un Entero.
+            }while (Integer.parseInt(resp) != 0); // Comparamos la respuesta, si no es 0 volvemos a pedir otro plus.
+            
+            JOptionPane.showMessageDialog(null, 
+                    """
+                    <html>
+                        <hr />
+                        <p>
+                            Pluses: %.2f€ <br />
+                            Sueldo Bruto: %.2f€ <br />
+                            IRPF: %.1f%%     Importe IRPF: %.2f€ <br />
+                            Sueldo Neto: %.2f€ <br />
+                        </p>
+                        <hr />
+                    </html>
+                    """.formatted(xx.pluses, xx.getSueldoBruto(), xx.getPorcIRPF(), xx.getImporteIRPF(), xx.getSueldoNeto()),
+                    "Datos de %s".formatted(xx.nombre),
+                    JOptionPane.INFORMATION_MESSAGE);
+             
+            resp = JOptionPane.showInputDialog(null, 
+                    """
+                    <html>
+                        <h1>¿Desea Introducir un Nuevo Empleado?</h1>
+                        <hr />
+                        <p>
+                            1 : SI <br />
+                            2 : SALIR
+                        </p>
+                    </html>
+                    """);
+            
+        }while (Integer.parseInt(resp) == 1);
     }
 }
--- Actividad4_2/src/main/java/org/zabalburu/daw1/actividad4_2/modelo/Empleado.java
+++ Actividad4_2/src/main/java/org/zabalburu/daw1/actividad4_2/modelo/Empleado.java
@@ -4,15 +4,160 @@
  */
 package org.zabalburu.daw1.actividad4_2.modelo;
 
+import javax.swing.JOptionPane;
+
 /**
  *
- * @author DAW1
+ * @author Iker Navarro Pérez
  */
 public class Empleado {
-    
+
     public String nombre;
     public int tipo;
     public int hijos;
     public double pluses;
-    
+
+    public String getDescripción() {
+
+        int x = 0; //Contador que hará que se repita el WHILE hasta que realize el IF.
+        String tipoText = "";
+
+        while (x == 0) { //Como x es igual a 0 entramos en el bucle, y dependiendo del tipo se repetirá o saldremos de la repetitiva.
+            if (tipo <= 4 && tipo >= 1) {
+                switch (tipo) {
+                    case 1:
+                        tipoText = "Operario";
+                        break;
+                    case 2:
+                        tipoText = "Obrero Especialista";
+                        break;                                     // Asignamos una descripción dependiendo del TIPO.
+                    case 3:
+                        tipoText = "Administrativo";
+                        break;
+                    default:
+                        tipoText = "Licenciado";
+                }
+                x++; // Suma 1 a x y sale de la repetitiva.
+            } else { // Entramos en caso de que el TIPO no sea el esperado en el IF
+                String resp = JOptionPane.showInputDialog(null, //Pedimos nuevamente el TIPO
+                        """
+                    <html>
+                        <h1>Tipo de Empleado No Válido</h1>
+                        <hr />
+                        <p>
+                            1 : Operario <br />
+                            2 : Obrero Especialista <br />
+                            3 : Administrativo <br />
+                            4 : Licenciado <br />
+                        </p> 
+                    </html>
+                    """);
+                tipo = Integer.parseInt(resp); // Se lo asignamos y volvemos a la repetitiva a comprobar si está dentro de los parámetros.
+            }
+        }
+        return tipoText; // Devolvemos la descripción.
+    }
+
+    public double getSueldoBase() {
+
+        int x = 0; //Contador que hará que se repita el WHILE hasta que realize el IF.
+        double sueldoBase = 0;
+
+        while (x == 0) { //Como x es igual a 0 entramos en el bucle, y dependiendo del tipo se repetirá o saldremos de la repetitiva.
+            if (tipo <= 4 && tipo >= 1) {
+
+                switch (tipo) {
+                    case 1:
+                        sueldoBase = 1200;
+                        break;
+                    case 2:
+                        sueldoBase = 1450;
+                        break;                                     // Asignamos un Sueldo Base dependiendo del TIPO.
+                    case 3:
+                        sueldoBase = 1300;
+                        break;
+                    default:
+                        sueldoBase = 1450;
+                }
+                x++; // Suma 1 a x y sale de la repetitiva.
+            } else { // Entramos en caso de que el TIPO no sea el esperado en el IF
+                String resp = JOptionPane.showInputDialog(null, //Pedimos nuevamente el TIPO
+                        """
+                    <html>
+                        <h1>Tipo de Empleado No Válido</h1>
+                        <hr />
+                        <p>
+                            1 : Operario <br />
+                            2 : Obrero Especialista <br />
+                            3 : Administrativo <br />
+                            4 : Licenciado <br />
+                        </p>
+                    </html>
+                    """);
+                tipo = Integer.parseInt(resp); // Se lo asignamos y volvemos a la repetitiva a comprobar si está dentro de los parámetros.
+            }
+        }
+        return sueldoBase; // Devolvemos el sueldo base.
+    }
+
+    public void nuevoPlus(double plus) {
+        pluses += plus; // Sumamos y asignamos el nuevo plus a los pluses existentes.
+    }
+
+    public double getSueldoBruto() {
+        return getSueldoBase() + pluses; // Devolvemos el Sueldo base con los pluses.
+    }
+
+    public double getPorcIRPF() {
+
+        double porcIRPF = 0; //Aquí asignaremos un valor dependiendo de los siguintes factores...
+
+        switch (hijos) { // Primero revisamos cuantos hijos tiene, y dependiendo de cuantos tenga y el sueldo bruto asignaremos al porcentaje de IRPF un valor u otro.
+            case 0: // CON 0 HIJOS
+                if (getSueldoBruto() < 1350) {
+                    porcIRPF = 13;
+                } else if (getSueldoBruto() >= 1350 && getSueldoBruto() < 1450) {
+                    porcIRPF = 14;
+                } else {
+                    porcIRPF = 17;
+                }
+                break;
+            case 1: // CON 1 HIJO
+                if (getSueldoBruto() < 1350) {
+                    porcIRPF = 12.5;
+                } else if (getSueldoBruto() >= 1350 && getSueldoBruto() < 1450) {
+                    porcIRPF = 13;
+                } else {
+                    porcIRPF = 16;
+                }
+                break;
+            case 2: // CON 2 HIJOS
+                if (getSueldoBruto() < 1350) {
+                    porcIRPF = 11;
+                } else if (getSueldoBruto() >= 1350 && getSueldoBruto() < 1450) {
+                    porcIRPF = 12;
+                } else {
+                    porcIRPF = 15;
+                }
+                break;
+            default: // Por defecto, es decir que no sean ni 0 ni 1 ni 2 hijos, 3 o +.
+                if (getSueldoBruto() < 1350) {
+                    porcIRPF = 10;
+                } else if (getSueldoBruto() >= 1350 && getSueldoBruto() < 1450) {
+                    porcIRPF = 11.5;
+                } else {
+                    porcIRPF = 14.5;
+                }
+        }
+        return porcIRPF; // Devolvemos el porcentaje.
+    }
+
+    public double getImporteIRPF() {
+        return getSueldoBruto() * (getPorcIRPF() / 100); //Sacamos el importe de IRPF.
+    }
+
+    public double getSueldoNeto() {
+        return getSueldoBruto() - getImporteIRPF(); // sacamos el Sueldo Neto.
+    }
+
 }
