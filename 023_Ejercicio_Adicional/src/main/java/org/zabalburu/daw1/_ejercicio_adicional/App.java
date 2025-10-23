9467cb9f74ed3baa3fcaf135c592f98185126832
Iker Navarro Pérez <iker.navarro.perez@zabalburu.org>
2025-10-23 21:03:35 +0200

    Creado 023_Ejercicio_Adicional

---
+++ 023_Ejercicio_Adicional/src/main/java/org/zabalburu/daw1/_ejercicio_adicional/App.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.zabalburu.daw1._ejercicio_adicional;
+
+/**
+ *
+ * @author Iker Navarro Pérez
+ * 
+ */
+public class App {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ 023_Ejercicio_Adicional/src/main/java/org/zabalburu/daw1/_ejercicio_adicional/modelo/Envio.java
@@ -0,0 +1,112 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.zabalburu.daw1._ejercicio_adicional.modelo;
+
+/**
+ *
+ * @author Iker Navarro Pérez
+ */
+public class Envio {
+
+    private String destinatario;
+    private String remitente;
+    private int pesoEnGramos;
+    private int tipoEnvio;
+    final static int TIPO_ENVIO_NORMAL = 1;
+    final static int TIPO_ENVIO_EXPRESS = 2;
+    final static int TIPO_ENVIO_24_HORAS = 3;
+    final static int TIPO_ENVIO_8_HORAS = 4;
+
+    public String getDestinatario() {
+        return destinatario;
+    }
+
+    public void setDestinatario(String destinatario) {
+        this.destinatario = destinatario;
+    }
+
+    public String getRemitente() {
+        return remitente;
+    }
+
+    public void setRemitente(String remitente) {
+        this.remitente = remitente;
+    }
+
+    public int getPesoEnGramos() {
+        return pesoEnGramos;
+    }
+
+    public void setPesoEnGramos(int pesoEnGramos) {
+        this.pesoEnGramos = pesoEnGramos;
+    }
+
+    public int getTipoEnvio() {
+        return tipoEnvio;
+    }
+
+    public void setTipoEnvio(int tipoEnvio) {
+        this.tipoEnvio = tipoEnvio;
+    }
+
+    public String getDescripcionTipo() {
+        String descripcionTipo = "";
+        if (tipoEnvio >= 1 && tipoEnvio <= 4) {
+            descripcionTipo = switch (tipoEnvio) {
+                case 1 ->
+                    "Normal";
+                case 2 ->
+                    "Express";
+                case 3 ->
+                    "24 Horas";
+                default ->
+                    "8 Horas";
+            };
+        }
+        return descripcionTipo;
+    }
+
+    public double getCosteBase() {
+        double costeBase = 0;
+        if (tipoEnvio >= 1 && tipoEnvio <= 4) {
+            costeBase = switch (tipoEnvio) {
+                case 1 -> 7;
+                case 2 -> 10;
+                case 3 -> 12;
+                default -> 15;
+            };
+        }
+        return costeBase;
+    }
+
+    public double getCoste200() {
+        double coste200 = 0;
+        if (tipoEnvio >= 1 && tipoEnvio <= 4) {
+            coste200 = switch (tipoEnvio) {
+                case 1 -> 0.5;
+                case 2 -> 0.7;
+                case 3 -> 0.8;
+                default -> 0.9;
+            };
+        }
+        return coste200;
+    }
+
+    public double getCostePeso() {
+        double costePeso = 0;
+        if (pesoEnGramos % 200 == 0){
+            costePeso = ((pesoEnGramos / 200) * this.getCoste200());
+        }else{
+            costePeso = (((pesoEnGramos / 200) + 1) * this.getCoste200());
+        }
+        return costePeso;
+    }
+    
+    public double getCoste(){
+        return this.getCosteBase() + this.getCostePeso();
+    }
+    
+    
+}
