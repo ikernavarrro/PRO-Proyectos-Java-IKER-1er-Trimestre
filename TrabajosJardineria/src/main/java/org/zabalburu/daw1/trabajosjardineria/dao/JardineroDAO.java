e9b51dc5e627d6bfde485aa98d3db3c7463fcc6f
ikernavarrro <iker.navarro.perez@zabalburu.org>
2025-11-14 13:59:46 +0100

    Modificado TrabajosJardineria

---
+++ TrabajosJardineria/src/main/java/org/zabalburu/daw1/trabajosjardineria/dao/JardineroDAO.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package org.zabalburu.daw1.trabajosjardineria.dao;
+
+import org.zabalburu.daw1.trabajosjardineria.modelo.Jardinero;
+
+/**
+ *
+ * @author Iker Navarro Pérez
+ */
+public interface JardineroDAO {
+    public Jardinero addJardinero(Jardinero nuevo);
+    public void deleteJardinero(Jardinero eliminar);
+    public void deleteJardinero(Integer id);
+    public void modifyJardinero(Jardinero modificar);
+}
