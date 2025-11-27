a5258159918028b4cb54c359977f50f45efdcbaa
ikernavarrro <iker.navarro.perez@zabalburu.org>
2025-11-27 21:07:34 +0100

    Terminada Actividad26

--- 2do-Trimestre/Actividad26/src/main/java/org/zabalburu/daw1/actividad26/FrmActualizarPerfil.java
+++ 2do-Trimestre/Actividad26/src/main/java/org/zabalburu/daw1/actividad26/FrmActualizarPerfil.java
@@ -17,6 +17,7 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.border.BevelBorder;
 
 /**
  *
@@ -44,15 +45,20 @@ public class FrmActualizarPerfil extends JFrame {
     
     //PANELES
     private JPanel pnlDatos1 = new JPanel(new BorderLayout());
+    private JPanel pnlBordeInferior1 = new JPanel();
     private JPanel pnlDatos2 = new JPanel(new BorderLayout());
+    private JPanel pnlBordeInferior2 = new JPanel();
     private JPanel pnlDatos3 = new JPanel(new BorderLayout());
+    private JPanel pnlBordeInferior3 = new JPanel();
     private JPanel pnlDatos4 = new JPanel(new BorderLayout());
+    private JPanel pnlBordeInferior4 = new JPanel();
     private JPanel pnlDatos5 = new JPanel(new BorderLayout());
+    private JPanel pnlBordeInferior5 = new JPanel();
     private JPanel pnlDatosMain = new JPanel(new GridLayout(5, 1, 0, 15));
     private JPanel pnlBotones = new JPanel(new FlowLayout(FlowLayout.RIGHT, 20, 15));
 
     //Dimensiones Ventana
-    private Dimension dmVentana = new Dimension(600, 800);
+    private Dimension dmVentana = new Dimension(800, 650);
 
     //Fuentes
     private Font fntVerdana = new Font("Verdana", Font.PLAIN, 18);
@@ -77,38 +83,89 @@ public class FrmActualizarPerfil extends JFrame {
         lblTitulo.setBackground(clrAzul);
         lblTitulo.setBorder(BorderFactory.createEmptyBorder(0, 20, 0, 0));
         lblTitulo.setOpaque(true);
-
+        
+        //pnlBordeInferior 1,2,3,4,5 (BorderLayout)
+        Dimension borde = new Dimension(0, 2);
+        //1
+        pnlBordeInferior1.setBackground(clrAzul);
+        pnlBordeInferior1.setPreferredSize(borde);
+        pnlBordeInferior1.setOpaque(true);
+        //2
+        pnlBordeInferior2.setBackground(clrAzul);
+        pnlBordeInferior2.setPreferredSize(borde);
+        pnlBordeInferior2.setOpaque(true);
+        //3
+        pnlBordeInferior3.setBackground(clrAzul);
+        pnlBordeInferior3.setPreferredSize(borde);
+        pnlBordeInferior3.setOpaque(true);
+        //4
+        pnlBordeInferior4.setBackground(clrAzul);
+        pnlBordeInferior4.setPreferredSize(borde);
+        pnlBordeInferior4.setOpaque(true);
+        //5
+        pnlBordeInferior5.setBackground(clrAzul);
+        pnlBordeInferior5.setPreferredSize(borde);
+        pnlBordeInferior5.setOpaque(true);
+        
+        
+        
         //pnlDatos1 (BorderLayout)
+        lblNombre.setBorder(BorderFactory.createEmptyBorder(10, 0, 5, 0));
         pnlDatos1.add(lblNombre, BorderLayout.NORTH);
-        pnlDatos1.add(txtNombre, BorderLayout.SOUTH);
+        txtNombre.setBorder(BorderFactory.createEmptyBorder(0, 5, 0, 5));
+        pnlDatos1.add(txtNombre, BorderLayout.CENTER);
+        pnlDatos1.add(pnlBordeInferior1, BorderLayout.SOUTH);
         
 
         //pnlDatos2 (BorderLayout)
+        lblCorreo.setBorder(BorderFactory.createEmptyBorder(10, 0, 5, 0));
         pnlDatos2.add(lblCorreo, BorderLayout.NORTH);
-        pnlDatos2.add(txtCorreo, BorderLayout.SOUTH);
+        txtCorreo.setBorder(BorderFactory.createEmptyBorder(0, 5, 0, 5));
+        pnlDatos2.add(txtCorreo, BorderLayout.CENTER);
+        pnlDatos2.add(pnlBordeInferior2, BorderLayout.SOUTH);
 
         //pnlDatos3 (BorderLayout)
+        lblTelefono.setBorder(BorderFactory.createEmptyBorder(10, 0, 5, 0));
         pnlDatos3.add(lblTelefono, BorderLayout.NORTH);
-        pnlDatos3.add(txtTelefono, BorderLayout.SOUTH);
+        txtTelefono.setBorder(BorderFactory.createEmptyBorder(0, 5, 0, 5));
+        pnlDatos3.add(txtTelefono, BorderLayout.CENTER);
+        pnlDatos3.add(pnlBordeInferior3, BorderLayout.SOUTH);
 
         //pnlDatos4 (BorderLayout)
+        lblDireccion.setBorder(BorderFactory.createEmptyBorder(10, 0, 5, 0));
         pnlDatos4.add(lblDireccion, BorderLayout.NORTH);
-        pnlDatos4.add(txtDireccion, BorderLayout.SOUTH);
+        txtDireccion.setBorder(BorderFactory.createEmptyBorder(0, 5, 0, 5));
+        pnlDatos4.add(txtDireccion, BorderLayout.CENTER);
+        pnlDatos4.add(pnlBordeInferior4, BorderLayout.SOUTH);
 
         //pnlDatos5 (BorderLayout)
+        lblSitioWeb.setBorder(BorderFactory.createEmptyBorder(10, 0, 5, 0));
         pnlDatos5.add(lblSitioWeb, BorderLayout.NORTH);
-        pnlDatos5.add(txtSitioWeb, BorderLayout.SOUTH);
+        txtSitioWeb.setBorder(BorderFactory.createEmptyBorder(0, 5, 0, 5));
+        pnlDatos5.add(txtSitioWeb, BorderLayout.CENTER);
+        pnlDatos5.add(pnlBordeInferior5, BorderLayout.SOUTH);
 
         //pnlDatosMain (GridLayout)
-        pnlDatosMain.add(pnlDatos5);
-        pnlDatosMain.add(pnlDatos4);
-        pnlDatosMain.add(pnlDatos3);
-        pnlDatosMain.add(pnlDatos2);
+        pnlDatosMain.setBorder(BorderFactory.createEmptyBorder(20, 30, 20, 30));
         pnlDatosMain.add(pnlDatos1);
+        pnlDatosMain.add(pnlDatos2);
+        pnlDatosMain.add(pnlDatos3); 
+        pnlDatosMain.add(pnlDatos4);
+        pnlDatosMain.add(pnlDatos5);
         
         //pnlBotones (FlowLayout)
-        pnlBotones.add(btnGuardar);
+        btnCancelar.setBorder(BorderFactory.createEmptyBorder(10, 15, 10, 15)); 
+        btnCancelar.setFont(fntVerdana.deriveFont(Font.BOLD, 12f));
+        btnCancelar.setForeground(Color.WHITE);
+        btnCancelar.setBackground(Color.LIGHT_GRAY);
         pnlBotones.add(btnCancelar);
+        btnGuardar.setBorder(BorderFactory.createEmptyBorder(10, 15, 10, 15)); 
+        btnGuardar.setFont(fntVerdana.deriveFont(Font.BOLD, 12f));
+        btnGuardar.setForeground(Color.WHITE);
+        btnGuardar.setBackground(clrAzul);
+        pnlBotones.setBorder(BorderFactory.createEmptyBorder(0, 0, 10, 20));
+        pnlBotones.add(btnGuardar);
+        
         
         //VENTANA PRINCIPAL
         this.add(lblTitulo, BorderLayout.NORTH);
