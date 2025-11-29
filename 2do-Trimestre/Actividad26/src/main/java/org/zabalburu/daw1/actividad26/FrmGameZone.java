4155ebed5589413572beb3553c130e0d706c1e2f
ikernavarrro <iker.navarro.perez@zabalburu.org>
2025-11-29 13:53:29 +0100

    Terminada Actividad26, Punto 1 y 2

--- 2do-Trimestre/Actividad26/src/main/java/org/zabalburu/daw1/actividad26/FrmGameZone.java
+++ 2do-Trimestre/Actividad26/src/main/java/org/zabalburu/daw1/actividad26/FrmGameZone.java
@@ -7,6 +7,7 @@ package org.zabalburu.daw1.actividad26;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.GridLayout;
 import javax.swing.BorderFactory;
@@ -16,6 +17,7 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
+import javax.swing.border.BevelBorder;
 
 /**
  *
@@ -31,29 +33,29 @@ public class FrmGameZone extends JFrame {
     JLabel lblPassword = new JLabel("PASSWORD");
     JPasswordField pswPassWord = new JPasswordField();
     JButton btnCancelar = new JButton("CANCELAR");
-    JButton btnResgistrarse = new JButton("REGISTRARSE");
+    JButton btnRegistrarse = new JButton("REGISTRARSE");
     
     //PANELES
     JPanel pnlGamertag = new JPanel(new BorderLayout());
     JPanel pnlEmail = new JPanel(new BorderLayout());
     JPanel pnlPassword = new JPanel(new BorderLayout());
     JPanel pnlDatos = new JPanel(new GridLayout(3, 1, 20, 10));
-    JPanel pnlBotones = new JPanel(); // Por defecto > FlowLayout
+    JPanel pnlBotones = new JPanel(new FlowLayout(FlowLayout.CENTER, 25, 15)); // Por defecto > FlowLayout,  Pero queremos ponerle alineamiento y HGAP, VGAP
     
     //DIMENSIONES
-    Dimension dmVentana = new Dimension(600, 600);
+    Dimension dmVentana = new Dimension(600, 500);
     
     //FUENTES
     Font fntImpact = new Font("Impact", Font.PLAIN, 18);
+    Font fntArial = new Font("Arial", Font.BOLD, 16);
     
     //COLORES
-    Color clrVerde = new Color(20, 255, 156);
-    Color clrRojo = new Color(255, 64, 64);
-    Color clrFondoOscuro = new Color(7, 27, 43);
-    Color clrFondoClaro = new Color(50, 65, 84);
-    
-    //CONSTRUCTOR
+    Color clrVerde = new Color(0, 255, 171);
+    Color clrRojo = new Color(255, 50, 80);
+    Color clrFondoOscuro = new Color(25, 25, 35);
+    Color clrFondoClaro = new Color(49, 50, 70);
 
+    //CONSTRUCTOR
     public FrmGameZone() {
         //CONFIGURACIÓN VENTANA
         this.setSize(dmVentana);
@@ -65,12 +67,67 @@ public class FrmGameZone extends JFrame {
         //TITULO pnlTitulo
         lblTitulo.setFont(fntImpact.deriveFont(Font.BOLD, 50f));
         lblTitulo.setHorizontalAlignment(JLabel.CENTER);
-        lblTitulo.setBorder(BorderFactory.createEmptyBorder(5, 0, 5, 0));
+        lblTitulo.setBorder(BorderFactory.createMatteBorder(0, 0, 3, 0, clrVerde));
         lblTitulo.setBackground(clrFondoClaro);
         lblTitulo.setOpaque(true);
         lblTitulo.setForeground(clrVerde);
-        //CONTENIDO pnlDatos 
+        
+        //CONTENIDO pnlDatos
+        lblGamertag.setBackground(clrFondoOscuro);
+        lblGamertag.setOpaque(true);
+        lblGamertag.setForeground(clrVerde);
+        lblGamertag.setFont(fntArial);
+        lblGamertag.setBorder(BorderFactory.createEmptyBorder(5, 0, 5, 0));
+        pnlGamertag.add(lblGamertag, BorderLayout.NORTH);
+        txtGamertag.setOpaque(true);
+        txtGamertag.setBorder(BorderFactory.createLineBorder( clrVerde, 1));
+        txtGamertag.setBackground(clrFondoClaro);
+        pnlGamertag.add(txtGamertag, BorderLayout.CENTER);
+        
+        lblEmail.setBackground(clrFondoOscuro);
+        lblEmail.setOpaque(true);
+        lblEmail.setForeground(clrVerde);
+        lblEmail.setFont(fntArial);
+        lblEmail.setBorder(BorderFactory.createEmptyBorder(5, 0, 5, 0));
+        pnlEmail.add(lblEmail, BorderLayout.NORTH);
+        txtEmail.setOpaque(true);
+        txtEmail.setBorder(BorderFactory.createLineBorder( clrVerde, 1));
+        txtEmail.setBackground(clrFondoClaro);
+        pnlEmail.add(txtEmail, BorderLayout.CENTER);
+        
+        lblPassword.setBackground(clrFondoOscuro);
+        lblPassword.setOpaque(true);
+        lblPassword.setForeground(clrVerde);
+        lblPassword.setFont(fntArial);
+        lblPassword.setBorder(BorderFactory.createEmptyBorder(5, 0, 5, 0));
+        pnlPassword.add(lblPassword, BorderLayout.NORTH);
+        pswPassWord.setOpaque(true);
+        pswPassWord.setBorder(BorderFactory.createLineBorder( clrVerde, 1));
+        pswPassWord.setBackground(clrFondoClaro);
+        pnlPassword.add(pswPassWord, BorderLayout.CENTER);
+        
+        pnlDatos.setBorder(BorderFactory.createEmptyBorder(30, 40, 30, 40));
+        pnlDatos.setBackground(clrFondoOscuro);
+        pnlDatos.setOpaque(true);
+        pnlDatos.add(pnlGamertag);
+        pnlDatos.add(pnlEmail);
+        pnlDatos.add(pnlPassword);
+        
         //BOTONES pnlBotones 
+        pnlBotones.setBackground(clrFondoOscuro);
+        pnlBotones.setOpaque(true);
+        btnRegistrarse.setBackground(clrVerde);
+        btnRegistrarse.setOpaque(true);
+        btnRegistrarse.setForeground(clrFondoOscuro);
+        btnRegistrarse.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createLineBorder(clrVerde.darker(), 2), BorderFactory.createEmptyBorder(10, 15, 10, 15)));
+        btnRegistrarse.setFont(fntArial);
+        pnlBotones.add(btnRegistrarse);
+        btnCancelar.setBackground(clrRojo);
+        btnCancelar.setOpaque(true);
+        btnCancelar.setForeground(clrFondoOscuro);
+        btnCancelar.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createLineBorder(clrRojo.darker(), 2), BorderFactory.createEmptyBorder(10, 15, 10, 15)));
+        btnCancelar.setFont(fntArial);
+        pnlBotones.add(btnCancelar);
         
         //VENTANA PRINCIPAL
         this.add(lblTitulo, BorderLayout.NORTH);
