2765657008161566a3766ec20a044a6f49cd8a25
Iker Navarro Pérez <iker.navarro.perez@zabalburu.org>
2025-12-30 11:01:02 +0100

    Actualizado FlintBibliotecaLibro_1 implementado DAO de Libro, Terminadas Implementaciones.

--- 2do-Trimestre/Flint Bliblioteca-Libro/FlintBibliotecaLibro_1/src/main/java/org/zabalburu/daw1/flintbibliotecalibro_1/dao/impl/LibroIMPL.java
+++ 2do-Trimestre/Flint Bliblioteca-Libro/FlintBibliotecaLibro_1/src/main/java/org/zabalburu/daw1/flintbibliotecalibro_1/dao/impl/LibroIMPL.java
@@ -14,41 +14,77 @@ import org.zabalburu.daw1.flintbibliotecalibro_1.modelo.Libro;
  * @author Iker Navarro Pérez
  */
 public class LibroIMPL implements LibroDAO {
-    
-    private List<Libro> libros;
+
+    private static List<Libro> libros;
 
     public LibroIMPL() {
         this.libros = new ArrayList<>();
     }
-    
+
     @Override
     public Libro addLibro(Libro nuevo) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        if (nuevo != null) {
+            Integer id = 1;
+            if (!libros.isEmpty()) {
+                id = libros.get(libros.size() - 1).getId() + 1;
+            }
+            nuevo.setId(id);
+            libros.add(nuevo);
+            return nuevo;
+        } else {
+            return null;
+        }
     }
 
     @Override
     public Libro getLibro(Integer id) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        int i;
+        for (i = 0; i < libros.size() && libros.get(i).getId() != id; i++);
+        if (i < libros.size()) {
+            return libros.get(i);
+        } else {
+            return null;
+        }
     }
 
     @Override
     public List<Libro> getLibros() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        return libros;
     }
 
     @Override
     public List<Libro> getLibrosBiblioteca(Integer idBiblioteca) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        List<Libro> resultado = new ArrayList<>();
+        for (Libro libro : libros) {
+            if (libro.getBiblioteca() != null && libro.getBiblioteca().getId().equals(idBiblioteca)) {
+                resultado.add(libro);
+            }
+        }
+        return resultado;
     }
 
     @Override
     public Libro modifyLibro(Libro modificar) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        int pos = libros.indexOf(modificar);
+        if (pos != -1) {
+            libros.set(pos, modificar);
+            return modificar;
+        } else {
+            return null;
+        }
     }
 
     @Override
     public Libro deleteLibro(Integer id) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        Libro b = new Libro();
+        b.setId(id);
+        int pos = libros.indexOf(b);
+        if (pos != -1) {
+            libros.remove(pos);
+            return b;
+        } else {
+            return null;
+        }
     }
-    
+
 }
